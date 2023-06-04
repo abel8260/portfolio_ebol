f330701f18d82671aa65908a557fe8a6132d97f8
abel vasconcelosb <78666074+abel8260@users.noreply.github.com>
2023-06-04 09:37:45 -0300

    rummer second up
    a try second

--- no1verde/oldie/pem/edu/index.php
+++ no1verde/oldie/pem/edu/index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 
-	<link rel="stylesheet" href="./style.css"/>
+	<head>
+		<link rel="stylesheet" href="./style.css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head><link rel="stylesheet" href="./style.css"/>
 
 
 	<body>
